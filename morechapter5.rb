be92e24ae1e65af2d2cd36ccaee1da468357547f
Johnathans <johnathan.smith@g5searchmarketing.com>
2013-12-07 14:49:09 -0800

    More exercies from chapter 5

---
+++ morechapter5.rb
@@ -0,0 +1,5 @@
+# Learn to program chapter 5 "Better Number Exercise"
+puts 'Hey! What\'s your favorite number?'
+fav_num = gets.chomp.to_i
+better_num = fav_num + 1
+puts 'That\'s ok, I guess, but isn\'t '+better_num.to_s+' just a bit better?'
